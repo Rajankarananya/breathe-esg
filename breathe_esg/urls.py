9626faf258edba85a148d3de2d9b1352a845182a
Rajankarananya <ananya.rajankar23@pccoepune.org>
2026-05-27 11:55:19 +0530

    fix SPA catch-all to serve static index.html

--- breathe_esg/urls.py
+++ breathe_esg/urls.py
@@ -1,7 +1,10 @@
 """URL configuration for breathe_esg."""
+import os
+
+from django.conf import settings as django_settings
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.views.generic import TemplateView
+from django.views.static import serve
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -9,5 +12,14 @@ urlpatterns = [
     path("api/", include("ingestion.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    re_path(r"^(?!api/|admin/).*", TemplateView.as_view(template_name="frontend/index.html")),
+    re_path(
+        r"^(?!api/|admin/).*$",
+        lambda request, *args, **kwargs: serve(
+            request,
+            "frontend/index.html",
+            document_root=django_settings.STATICFILES_DIRS[0]
+            if django_settings.STATICFILES_DIRS
+            else os.path.join(django_settings.BASE_DIR, "breathe_esg", "static"),
+        ),
+    ),
 ]
