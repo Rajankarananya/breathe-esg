1d814078b894b77acbf3467f2f9d390c03d5b68d
Rajankarananya <ananya.rajankar23@pccoepune.org>
2026-05-27 11:45:17 +0530

    fix prod API URL, add gitignore, remove staticfiles from tracking

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_URL || "https://breathe-esg-jg7b.onrender.com"
+
 const client = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL,
 })
 
 client.interceptors.request.use((config) => {
